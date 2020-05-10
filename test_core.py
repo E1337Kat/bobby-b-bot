2aa9601ad006350e0c099095ea6e33231dc642a3
voicelessreason <arlenstrausman@gmail.com>
2020-05-10 14:51:07 -0400

    Add a test for utils.core.

---
+++ test_core.py
@@ -0,0 +1,49 @@
+import unittest
+from unittest.mock import MagicMock
+
+
+class TestCore(unittest.TestCase):
+    MESSAGES = [
+        {
+            "TRIGGERS": ["trigger one"],
+            "RESPONSES": ["Response One"]
+        },
+        {
+            "TRIGGERS": ["trigger two"],
+            "RESPONSES": ["Response Two"]
+        }
+    ]
+
+    def test_get_random_quote(self):
+        from utils.core import get_random_quote
+
+        self.assertEqual(get_random_quote(self.MESSAGES[0]["RESPONSES"]), "Response One")
+
+    def test_is_keyword_mentioned(self):
+        from utils.core import is_keyword_mentioned
+
+        # Returns the correct value in each case
+        self.assertTrue(is_keyword_mentioned("trigger one", self.MESSAGES[0]["TRIGGERS"]))
+        self.assertFalse(is_keyword_mentioned("trigger one", self.MESSAGES[1]["TRIGGERS"]))
+
+        # Ignores capitalization
+        self.assertTrue(is_keyword_mentioned("TRIGGER TWO", self.MESSAGES[1]["TRIGGERS"]))
+        self.assertFalse(is_keyword_mentioned("TRIGGER TWO", self.MESSAGES[0]["TRIGGERS"]))
+
+    def test_generate_message_response(self):
+        from utils.core import generate_message_response
+
+        self.assertEqual(generate_message_response("trigger one", self.MESSAGES), "Response One")
+        self.assertEqual(generate_message_response("trigger two", self.MESSAGES), "Response Two")
+
+    def test_get_username(self):
+        from utils.core import get_username
+        author_mock = MagicMock()
+        author_mock.name = "Peter Parker"
+
+        self.assertEqual(get_username(author_mock), 'Peter Parker')
+        self.assertEqual(get_username(None), '[deleted]')
+
+
+if __name__ == '__main__':
+    unittest.main()
